4b06bc16ba342a6490e1ffdae9cd98ae50a611aa
Clay McGinnis <claymav@yahoo.com>
2017-02-13 15:16:01 -0600

    Added favicons, icons for apple and android devices, and all the necessary code to make it work.

--- ACM_General/ACM_General/settings.py
+++ ACM_General/ACM_General/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'localhost']
+ALLOWED_HOSTS = [u'localhost','mav.claymav.com']
 
 ENFORCED_EMAIL_DOMAINS = ['mst.edu']
 
