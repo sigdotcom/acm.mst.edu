2af950ec0a36ce7bf9b0eb99e53a145e90a9d37c
root <root@acm.mst.edu>
2017-02-19 19:46:09 -0600

    [UPDATE] Tested Complete Deployment

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install python3 python3-pip postgresql libpq-dev
+sudo apt-get install python3 python3-pip postgresql libpq-dev uwsgi uwsgi-plugin-python3
 sudo -u postgres psql -c "create database django_acmgeneral"
 sudo -u postgres psql -c "create user djangouser with password 'djangoUserPassword'"
 sudo -u postgres psql -c "grant all privileges on database django_acmgeneral to djangouser"
