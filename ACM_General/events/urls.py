ee0f926e689a77150bab9cdb78e6872c8e0da42f
Zachary Wileman <zdw27f@mst.edu>
2017-03-16 01:24:34 -0500

    [UPDATE]
    - Removed files that were originally apart of another branch and
      somehow became apart of the documentation branch.
    - Done so that the event files stay the same as masters

--- ACM_General/events/urls.py
+++ ACM_General/events/urls.py
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from events import views
+from django.conf.urls import url
+from . import views
 
 app_name = 'events'
 urlpatterns = [
-    # acm.mst.edu/events/
     url('^$', views.list_events, name='events-list'),
-
-    # acm.mst.edu/events/create/
-    url('create/$', views.create_event, name='create-event'),
 ]
--- ACM_General/events/views.py
+++ ACM_General/events/views.py
@@ -1,30 +1,10 @@
 from django.shortcuts import render
-from .models import Event
-from datetime import datetime
-from .forms import EventForm
-from django.http import HttpResponse
+from . import models
 
 
 def list_events(request):
     return(render(
         request,
         'events/listEvents.html',
-        {'eventsList': Event.objects.all()}
+        {'eventsList': models.Event.objects.all()}
     ))
-
-
-def create_event(request):
-    # if not request.user.is_authenticated():
-    #    return render(request, './accounts/templates/login.html')
-
-    if request.method == 'POST':
-        form = EventForm(request.POST)
-
-        if form.is_valid():
-            event = form.save(commit=False)
-            event.creator = request.user
-            event.save()
-        else:
-            return HttpResponse(form.errors)
-
-    return render(request, 'events/create-event.html', {'form': EventForm})
