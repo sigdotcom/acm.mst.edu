7c944756e63797d46631aa61a8ee8860229b76c9
Clay McGinnis <claymav@yahoo.com>
2017-03-19 19:34:34 -0500

    Added 404 page and stuff to facilitate it, hypothetically, without nginx on my system I cannot test. Removed login page, but instead direct link to oauth.

--- ACM_General/core/views.py
+++ ACM_General/core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
-# Create your views here.
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
