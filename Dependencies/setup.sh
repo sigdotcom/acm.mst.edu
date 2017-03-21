33fba9abe272b4c4863c5e7ab4b6f65bd6992d34
Kevin Schoonover <kevin.m.schoonover@gmail.com>
2017-03-21 23:31:41 +0000

    [UPDATE] added setup.sh features to make migrations easier

--- Dependencies/setup.sh
+++ Dependencies/setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install python3 python3-pip postgresql nginx libpq-dev uwsgi uwsgi-plugin-python3
+sudo -u postgres psql -c "drop database django_acmgeneral"
 sudo -u postgres psql -c "create database django_acmgeneral"
 sudo -u postgres psql -c "create user djangouser with password 'djangoUserPassword'"
 sudo -u postgres psql -c "grant all privileges on database django_acmgeneral to djangouser"
@@ -9,12 +10,14 @@ cp settings_local.template ../ACM_General/ACM_General/settings_local.py
 cp ACMGeneral_uwsgi.ini /etc/uwsgi/apps-available
 cp ssl-acm.mst.edu /etc/nginx/sites-available
 sudo ln -s /etc/uwsgi/apps-available/ACMGeneral_uwsgi.ini /etc/uwsgi/apps-enabled/
-# sudo ln -s /etc/nginx/sites-available/ssl-acm.mst.edu /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/ssl-acm.mst.edu /etc/nginx/sites-enabled/
 sudo rm /etc/nginx/sites-enabled/default
 cd ../ACM_General
 sudo chown www-data:www-data -R /var/django
-sudo python3 manage.py makemigrations accounts core events home payments rest_api sigs thirdparty_auth --noinput
+find .. -name migrations -exec rm -rf {} \;
+sudo python3 manage.py makemigrations accounts core events home sigs thirdparty_auth payments --noinput
 sudo python3 manage.py collectstatic --noinput
-sudo python3 manage.py migrate
+sudo python3 manage.py migrate --noinput
+sudo chown www-data:www-data -R /var/django
 sudo service uwsgi restart
 sudo service nginx restart
