6535d74574936e1be59059115a252d00df525c37
Kevin Schoonover <schoonoverkevinm@gmail.com>
2017-05-12 23:47:55 +0000

    [UPDATE] Comments and ambiguous starting location support to setup.sh

--- Dependencies/setup.sh
+++ Dependencies/setup.sh
@@ -1,11 +1,32 @@
+###
+# Working with the package manager to install all the necessary linux
+# dependencies.
+###
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install python3 python3-pip postgresql nginx libpq-dev uwsgi uwsgi-plugin-python3 xvfb
+
+###
+# Preparing the database
+###
 sudo -u postgres psql -c "drop database django_acmgeneral"
 sudo -u postgres psql -c "create database django_acmgeneral"
 sudo -u postgres psql -c "create user djangouser with password 'djangoUserPassword'"
 sudo -u postgres psql -c "grant all privileges on database django_acmgeneral to djangouser"
 sudo -u postgres psql -c "alter user djangouser createdb"
+
+###
+# Putting the main repository in /var/django/
+###
+mkdir -p /var/django/
+cd ../../
+cp -rf acm.mst.edu /var/django/
+cd /var/django/acm.mst.edu/Dependencies
+
+###
+# Setting up all the python packages and moving he propeer configuration files
+# into place.
+###
 sudo pip3 install -r requirements.txt
 # WARNING: This -n will not quash any existing files so if you're looking for a
 #          complete overwrite remove these flags
@@ -16,17 +37,27 @@ cp -n ssl-acm.mst.edu /etc/nginx/sites-available
 sudo ln -s /etc/uwsgi/apps-available/ACMGeneral_uwsgi.ini /etc/uwsgi/apps-enabled/
 sudo ln -s /etc/nginx/sites-available/ssl-acm.mst.edu /etc/nginx/sites-enabled/
 sudo rm /etc/nginx/sites-enabled/default
-cd /usr/local/bin/
-wget https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz
-tar -xvzf geckodriver-v0.15.0-linux64.tar.gz
-rm geckodriver-v0.15.0-linux64.tar.gz
-cd - 
 cd ../ACM_General
+###
+# www-data needs to own the directory for special nginx interactions
+###
 sudo chown www-data:www-data -R /var/django
+
+###
+# Generating the django migrations from scratch.
+###
 find .. -name migrations -type d -exec rm -rf {} \;
-python3 manage.py makemigrations accounts core events home sigs thirdparty_auth payments --noinput
+for d in *; do
+    if [ -d "$d" ]; then
+        echo "Running $D"
+        python3 manage.py makemigrations "$d"
+    fi
+done
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
-sudo chown www-data:www-data -R /var/django
+
+###
+# Restarting the two services necessary to make it run.
+###
 sudo service uwsgi restart
 sudo service nginx restart
