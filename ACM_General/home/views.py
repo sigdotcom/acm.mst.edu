0bce4ed697e0750f292c8f78533a35bf47827001
Clay McGinnis <mcginnis@claymav.com>
2017-08-01 18:09:42 -0500

    CHANGED color of footer, made the buttons work with stripe on membership, fixed mobile, and renamed 1/2 of all base.html files. ADDED newer version of footer image.

--- ACM_General/home/views.py
+++ ACM_General/home/views.py
@@ -1,45 +1,58 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def index(request):
-    return(render(
-        request,
-        'home/index.html',
-    ))
+    return (
+        render(
+            request,
+            'home/index.html',
+        )
+    )
 
 
 def sponsors(request):
-    return(render(
-        request,
-        'home/sponsors.html',
-    ))
+    return (
+        render(
+            request,
+            'home/sponsors.html',
+        )
+    )
 
 
 def calendar(request):
-    return(render(
-        request,
-        'home/calendar.html',
-    ))
+    return (
+        render(
+            request,
+            'home/calendar.html',
+        )
+    )
 
 
 def media(request):
-    return(render(
-        request,
-        'home/media.html',
-    ))
+    return (
+        render(
+            request,
+            'home/media.html',
+        )
+    )
 
 
 def officers(request):
-    return (render(
-        request,
-        'home/officers.html',
-    ))
+    return (
+        render(
+            request,
+            'home/officers.html',
+        )
+    )
 
 
 def membership(request):
-    return (render(
-        request,
-        'home/membership.html',
-    ))
+    return (
+        render(
+            request,
+            'home/membership.html',
+        )
+    )
