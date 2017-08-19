b5b5e049152849add273d3ce7618ec15856536e2
Kevin Schoonover <schoonoverkevinm@gmail.com>
2017-08-19 18:02:56 -0500

    [UPDATE] Attemping to fix failing sass compile

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
-css_dir = "/ACM_General/home/static/home/css" 
-sass_dir = "/ACM_General/home/static/home/scss" 
-images_dir = "/ACM_General/home/static/home/img" 
-javascripts_dir = "/ACM_General/home/static/home/js" 
+css_dir = "./ACM_General/home/static/home/css" 
+sass_dir = "./ACM_General/home/static/home/scss" 
+images_dir = "./ACM_General/home/static/home/img" 
+javascripts_dir = "./ACM_General/home/static/home/js" 
 output_style = :expanded 
 relative_assets = true 
 line_comments = true
