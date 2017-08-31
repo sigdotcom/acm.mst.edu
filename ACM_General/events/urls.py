a18e9b6297303e8d014ed553e5461310c3a91b9f
Zachary Wileman <zdw27f@mst.edu>
2017-08-30 22:13:13 -0500

    [UPDATE] Small documentation change
    - Removed general documentation on urls.py file inside of Events app.
      The documentation isn't specific to the event's url file so I have
      removed it from the file but kept it on my computer for when we decide
      to create a general documentation area (if we do).

--- ACM_General/events/urls.py
+++ ACM_General/events/urls.py
@@ -1,29 +1,3 @@
-"""
-ACM_General URL Configuration
------------------------------
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-https://docs.djangoproject.com/en/1.10/topics/http/urls/
-
-Examples:
-
-Function views
---------------
-#. Add an import:  from my_app import views
-#. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-
-Class-based views
------------------
-
-#. Add an import:  from other_app.views import Home
-#. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-
-Including another URLconf
--------------------------
-
-#. Import the include() function: from django.conf.urls import url, include
-#. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from events import views
 
