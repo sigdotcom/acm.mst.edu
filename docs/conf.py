bc4ba965ffed330073dda4d64e0c1851662d2748
Zachary Wileman <zdw27f@mst.edu>
2017-09-07 09:52:38 -0500

    Updated authors in the Sphinx config file since multiple people have done work on the documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -57,8 +57,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'MST-ACM Website (acm.mst.edu)'
-copyright = '2017, Kevin Schoonover, Zachary Wileman'
-author = 'Kevin Schoonover, Zachary Wileman'
+copyright = '2017, SIG.com'
+author = 'SIG.com'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -138,7 +138,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'MST-ACMWebsiteacmmstedu.tex', 'MST-ACM Website (acm.mst.edu) Documentation',
-     'Kevin Schoonover, Zachary Wileman', 'manual'),
+     'SIG.com', 'manual'),
 ]
 
 
