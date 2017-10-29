d43637d2628afe4d921ee2e570c2e435a1607fb7
Kevin Schoonover <schoonoverkevinm@gmail.com>
2017-10-29 15:52:13 -0500

    Deleting payments + updating main urls

--- ACM_General/ACM_General/urls.py
+++ ACM_General/ACM_General/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^social-auth/', include('thirdparty_auth.urls')),
     url(r'^web-api/', include('rest_api.urls')),
-    url(r'^account/', include('accounts.urls')),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^events/', include('events.urls')),
     url(r'^products/', include('products.urls')),
     url(r'^', include('home.urls')),
